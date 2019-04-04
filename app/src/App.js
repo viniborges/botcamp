6deb3a4ff197fd1723f0004da311d63e9614b2c5
Luis Vinicius <viniborges@yahoo.com.br>
2019-04-04 02:13:42 +0000

    aula 2

--- app/src/App.js
+++ app/src/App.js
@@ -2,16 +2,17 @@ import React, {Component} from 'react';
 
 import LogoBotcamp from './components/LogoBotcamp';
 import BtnBotcamp from "./components/BtnBotcamp";
+import LoginWrapper from './components/LoginWrapper';
 
 import "./styles/reset.css";
 
 class App extends Component {
     render(){
         return (
-            <>
+            <LoginWrapper>
                 <LogoBotcamp />
                 <BtnBotcamp />
-            </>
+            </LoginWrapper >
         );
     }
 }
---
+++ app/src/components/LoginWrapper/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import background from "../../img/ecossistema-background.png";
+
+const LoginWrapper = styled.main`
+    background-image: url(${background});
+    width: 100vw;
+    height: 100vh;
+`;
+
+export default LoginWrapper;
