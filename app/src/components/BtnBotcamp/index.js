e4265da0c8b5bcec67792f446f890d471e41e3ac
Luis Vinicius <viniborges@yahoo.com.br>
2019-04-04 02:32:42 +0000

    BtnBotcamp usando styled-components

--- app/src/components/BtnBotcamp/index.js
+++ app/src/components/BtnBotcamp/index.js
@@ -1,8 +1,20 @@
-import React from "react";
-import "./style.css"
+import styled from "styled-components";
 
-const BtnBotcamp = props => (
-    <button className="btn-botcamp">{props.children}</button>
-);
+const BtnBotcamp = styled.button `
+    width: 280px;
+    height: 50px;
+    font-size: 20px;
+    border-radius: 25px;
+    background-color: crimson;
+    color:white;
+    text-transform: uppercase;
+`;
+
+// import React from "react";
+// import "./style.css"
+
+// const BtnBotcamp = props => (
+//     <button className="btn-botcamp">{props.children}</button>
+// );
 
 export default BtnBotcamp;
