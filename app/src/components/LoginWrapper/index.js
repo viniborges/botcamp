f16adbd0d1542e96d66979f3e0c39b632515e6c3
Luis Vinicius <viniborges@yahoo.com.br>
2019-04-04 02:53:18 +0000

    Aula03 40 - Posicionar as coisas

--- app/src/components/LoginWrapper/index.js
+++ app/src/components/LoginWrapper/index.js
@@ -4,8 +4,16 @@ import background from "../../img/ecossistema-background.png";
 
 const LoginWrapper = styled.main`
     background-image: url(${background});
+    box-sizing: border-box;
     width: 100vw;
     height: 100vh;
+    padding-top: 120px;
+    padding-bottom: 80px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
 `;
 
 export default LoginWrapper;
